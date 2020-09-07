80a3107cd8508dc161655af17a2496ca3667fb9e
Mercy Musa <memusa5h@gmail.com>
2020-09-07 20:23:38 +0000

    Add filter in Servlet

---
+++ src/com/simplilearn/filter/AuthFilter.java
@@ -0,0 +1,56 @@
+package com.simplilearn.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class AuthFilter
+ */
+@WebFilter("/hello")
+public class AuthFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public AuthFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		System.out.println("Destroy Filter");
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		String email =request.getParameter("email");
+		String password =request.getParameter("password");
+		response.setContentType("text/html");
+		if(email.equals("admin@gmail.com")) {
+			    if(password.equals("password123")) {
+			   chain.doFilter(request, response);
+		}//end if
+		else {response.getWriter().println("<h2>User Not  Found!....</h2><p> looking For Default User admin@gmail.com</p>");
+		}
+	
+		
+		}
+	
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {}
+
+}
